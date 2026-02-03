f88af90361f2312b444f883861e5904fb4039c3b
ThatBaldDude4 <taylorv0004@gmail.com>
2026-02-03 14:16:09 -0600

    renderbooks function proplerly renders book data

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 const libraryContainer = document.getElementById("library-container")
 
-const myLibrary = [];
+const state = {
+    view: "library", // library || edit form
+    library: [], // storage for book objects
+}
+
+function render(state) {
+    if (state.view === "library") {
+        // function renderLibrary()
+    }
+    if (state.view === "edit") {
+        // function renderEditForm()
+    }
+    if (state.view === "create") {
+        // function renderCreateForm()
+    }
+}
 
 function Book(title, author, pages, hasRead, id) {
     this.title = title;
@@ -10,27 +25,46 @@ function Book(title, author, pages, hasRead, id) {
     this.id = id;
 }
 
-function addBookToLibrary(title, author, pages) {
+function addBookToLibrary(title, author, pages, hasRead) {
     const uniqueId = crypto.randomUUID();
     const newBook = new Book(title, author, pages, hasRead, uniqueId);
 
-    myLibrary.push(newBook)
+    state.library.push(newBook)
 }
 
-function displayBooks() {
-    const bookContainerEl = document.createElement("div");
-    const titleEl = document.createElement("h2");
-    const authorEl = document.createElement("h3");
-    const pageCountEl = document.createElement("div");
-    const hasReadEl = document.createElement("div");
-    const editBtn = document.createElement("button");
-    const deleteBtn = document.createElement("button");
+function renderBooks() {
+    let booksArr = state.library;
 
+    for (let {title, author, pages, hasRead, id} of booksArr) {
+        let bookContainerEl = document.createElement("div");
+        let titleEl = document.createElement("h2");
+        let authorEl = document.createElement("h3");
+        let pageCountEl = document.createElement("div");
+        let hasReadEl = document.createElement("div");
+        let editBtn = document.createElement("button");
+        let deleteBtn = document.createElement("button");
 
-    myLibrary.forEach(({title, author, pages, hasRead, id}) => {
         titleEl.textContent = title;
         authorEl.textContent = author;
         pageCountEl.textContent = pages;
         hasReadEl.textContent = hasRead ? "Have read" : "Not Read";
-    })
+        editBtn.textContent = "EDIT";
+        deleteBtn.textContent = "DELETE";
+
+        bookContainerEl.appendChild(titleEl);
+        bookContainerEl.appendChild(authorEl);
+        bookContainerEl.appendChild(pageCountEl);
+        bookContainerEl.appendChild(hasReadEl);
+        bookContainerEl.appendChild(editBtn);
+        bookContainerEl.appendChild(deleteBtn);
+
+        libraryContainer.appendChild(bookContainerEl)
+    }
 }
+
+addBookToLibrary("Title1", "Taylor", 200, false)
+addBookToLibrary("Title2", "Taylor", 200, false)
+addBookToLibrary("Title3", "Taylor", 200, false)
+addBookToLibrary("Title4", "Taylor", 200, false)
+
+renderBooks()
